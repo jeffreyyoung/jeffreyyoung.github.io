4bb80252ef4190d53d57590e089e7a79c35fd8b1
Jeffrey Young <jeffreeyoung@yahoo.com>
2018-02-25 16:31:44 -0700

    yeeee

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default (props) => (
     <Head>
       <link rel="stylesheet" href="https://unpkg.com/tachyons@4.9.1/css/tachyons.min.css"/>
     </Head>
-    <section className='bg-white pa3 pa4-ns flex flex-column justify-between '>
+    <section className='bg-white pt3 mr3 ml3 flex flex-column justify-between '>
       <div></div>
       <div>
         <h1 className='f1 fw3'>Hi 👋, I'm Jeffrey.</h1>
@@ -13,21 +13,23 @@ export default (props) => (
       </div>
       <div></div>
     </section>
-    <section className='pa3 pa4-ns'>
+    <section className='pt3 mr3 ml3'>
       <h2 className='f2 fw3'>Resume</h2>
       <ul>
-        <li><b>Software Engineer</b> Adobe, August 2016 - Present</li>
-        <li><b>Software Engineer Intern</b> Qualtrics, Jan 2015 - April 2016</li>
+        <li>Software Engineer <b>Adobe</b>, Aug 2016 - Present</li>
+        <li>Software Engineer Intern <b>Qualtrics</b>, Jan 2015 - April 2016</li>
+        <li>Analyst <b>Anglepoint Inc</b>, Oct 2014 - Jan 2015</li>
+        <li>Front-End Web Developer <b>BYU Broadcasting</b>, Sep 2013 - Oct 2014</li>
       </ul>
     </section>
-    <section className='pa3 pa4-ns'>
+    <section className='pt3 mr3 ml3'>
       <h2 className='f2 fw3'>Freelance Projects</h2>
       <ul>
         <li>PlenaData Home Page Animation ~ Jan 2018</li>
         <li>Whistic Home Page ~ Nov 2017</li>
       </ul>
     </section>
-    <section className='pa3 pa4-ns'>
+    <section className='pt3 mr3 ml3'>
       <h2 className='f2 fw3'>Blog Posts</h2>
       <ul>
         <li>Javascript Alternative to Animate Motion ~ Jan 2018</li>
