9911ed7ebda2eee958e3951345c471260f349ce7
Jeffrey Young <jeffreeyoung@yahoo.com>
2018-02-25 16:03:42 -0700

    yeee

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+export default (props) => (
+  <div>
+    <section>
+      <h1>Hi 👋, I'm Jeffrey.</h1>
+      <h2>I'm a software engineer and freelance web developer. 💻</h2>
+    </section>
+    <hr />
+    <section>
+      <h2>Resume</h2>
+      <ul>
+        <li><b>Software Engineer</b> Adobe, August 2016 - Present</li>
+        <li><b>Software Engineer Intern</b> Qualtrics, Jan 2015 - April 2016</li>
+      </ul>
+    </section>
+    <hr />
+    <section>
+      <h2>Freelance Projects</h2>
+      <ul>
+        <li>PlenaData Home Page Animation ~ Jan 2018</li>
+        <li>Whistic Home Page ~ Nov 2017</li>
+      </ul>
+    </section>
+    <section>
+      <h2>Blog Posts</h2>
+      <ul>
+        <li>Javascript Alternative to Animate Motion ~ Jan 2018</li>
+      </ul>
+    </section>
+  </div>
+)
