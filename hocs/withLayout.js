0c6a988da1768241835bec80b999bf62a98cd239
Jeffrey Young <jeyoung@adobe.com>
2018-03-03 01:40:39 -0700

    meow

--- hocs/withLayout.js
+++ hocs/withLayout.js
@@ -4,6 +4,7 @@ export default function withLayout(ComposedComponent) {
   return props => (
     <div className='sans-serif measure center f4 f3'>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://unpkg.com/tachyons@4.9.1/css/tachyons.min.css"/>
       </Head>
       <ComposedComponent {...props} />
