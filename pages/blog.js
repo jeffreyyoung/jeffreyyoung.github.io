2906590e47a7fce847f30f65b3746bea1d5f583a
Jeffrey Young <jeyoung@adobe.com>
2018-03-03 01:52:18 -0700

    yeee

--- pages/blog.js
+++ pages/blog.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 
 export default withData(withLayout(({post}) => (
-  <article className='pt4'>
+  <article className='pt4 mr3 ml3'>
     <Link href='/'><a>👈 Back</a></Link>
     <h1 className='green'>{post.meta.title}</h1>
     <div dangerouslySetInnerHTML={{__html:post.html}} />
--- pages/projects.js
+++ pages/projects.js
@@ -3,7 +3,7 @@ import withLayout from './../hocs/withLayout';
 import Link from 'next/link';
 
 export default withData(withLayout(({project}) => (
-  <article className='pt4'>
+  <article className='pt4 mr3 ml3'>
     <Link href='/'><a>👈 Back</a></Link>
     <h1 className='green'>{project.meta.title}</h1>
     <div dangerouslySetInnerHTML={{__html:project.html}} />
