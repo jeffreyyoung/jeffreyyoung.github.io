de9088d3d6e7ecc855e379f24764d819825d6378
Jeffrey Young <jeyoung@adobe.com>
2018-03-03 01:49:38 -0700

    meeeow

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,13 @@ export default withData(withLayout(({projects, posts}) => (
       <div></div>
       <div>
         <h1 className='f1 fw3'>Hi 👋, I'm Jeffrey.</h1>
-        <h2 className='f2 fw3'>I'm a software engineer and freelance web developer. 💻</h2>
+        <h2 className='f3 fw3'>I'm a software engineer and freelance web developer. 💻</h2>
       </div>
       <div></div>
     </section>
     <section className='pt3 mr3 ml3'>
-      <h2 className='f2 fw3'>Resume</h2>
-      <ul>
+      <h2 className='f2 fw3'>Resume 📋</h2>
+      <ul className='f4 fw3'>
         <li>Software Engineer <b>Adobe</b>, Aug 2016 - Present</li>
         <li>Software Engineer Intern <b>Qualtrics</b>, Jan 2015 - April 2016</li>
         <li>Analyst <b>Anglepoint Inc</b>, Oct 2014 - Jan 2015</li>
@@ -23,14 +23,14 @@ export default withData(withLayout(({projects, posts}) => (
       </ul>
     </section>
     <section className='pt3 mr3 ml3'>
-      <h2 className='f2 fw3'>Projects</h2>
-      <ul>
+      <h2 className='f2 fw3'>Projects 🛠</h2>
+      <ul className='f4 fw3'>
         {projects.map(p => (<li><Link href={p.href} as={p.url}><a>{p.title}</a></Link></li>))}
       </ul>
     </section>
     <section className='pt3 mr3 ml3'>
-      <h2 className='f2 fw3'>Blog</h2>
-      <ul>
+      <h2 className='f2 fw3'>Blog 📝</h2>
+      <ul className='f4 fw3'>
         {posts.map(p => (<li><Link href={p.href} as={p.url}><a>{p.title}</a></Link></li>))}
       </ul>
     </section>
